d6d54d4c3cb8fb657ce34c4d9a4cb27f7679cf2a
Chokun Rojanapron <chokun@chokunplayz.com>
2023-04-08 21:18:29 +0700

    added dust sensor support

--- src/HAIntegration.cpp
+++ src/HAIntegration.cpp
@@ -4,31 +4,42 @@
 #include <ArduinoHA.h>
 #include <WiFi.h>
 
-//Adapted via:
-//  https://github.com/dawidchyrzynski/arduino-home-assistant/blob/main/examples/nano33iot/nano33iot.ino
+// Adapted via:
+//   https://github.com/dawidchyrzynski/arduino-home-assistant/blob/main/examples/nano33iot/nano33iot.ino
 
 WiFiClient client;
 HADevice device;
 HAMqtt mqtt(client, device);
-HAFan fan("fan", HAFan::SpeedsFeature);
+HAFan fan("fanspeed", HAFan::SpeedsFeature);
+HASensorNumber dustSensor("dustsensor");
+
+unsigned long lastUpdateAt = 0;
 
 int lastspeed = 0;
 bool gstate = false;
 
-void HAIntegration::configure() {
+const int dustLed = 2;
+
+float voMeasured = 0;
+float calcVoltage = 0;
+float dustDensity = 0;
+
+void HAIntegration::configure()
+{
 
     Serial.println("Setting Up Home Assistant, Hang tight!");
 
     pinMode(LED_BUILTIN, OUTPUT);
     pinMode(0, OUTPUT);
-    
-    //Set device ID as MAC address
+    pinMode(dustLed, OUTPUT);
+
+    // Set device ID as MAC address
 
     byte mac[WL_MAC_ADDR_LENGTH];
     WiFi.macAddress(mac);
     device.setUniqueId(mac, sizeof(mac));
 
-    //Device metadata:
+    // Device metadata:
 
     device.setName("Atmosphere Connect");
     device.setSoftwareVersion("2.0");
@@ -41,40 +52,78 @@ void HAIntegration::configure() {
     fan.onStateCommand(stateHandler);
     fan.onSpeedCommand(speedHandler);
 
+    dustSensor.setName("Dust Density");
+    dustSensor.setIcon("mdi:air-filter");
+    dustSensor.setUnitOfMeasurement("g");
+
     Serial.print("Connecting to MQTT\n");
-    
-    if (mqtt.begin(MQTT_BROKER, MQTT_LOGIN, MQTT_PASSWORD) == true) {
+
+    if (mqtt.begin(MQTT_BROKER, MQTT_LOGIN, MQTT_PASSWORD) == true)
+    {
         Serial.print("Connected to MQTT broker");
-    } else {
+    }
+    else
+    {
         Serial.print("Could not connect to MQTT broker");
     }
 }
 
-void HAIntegration::stateHandler(bool state, HAFan* sender) {
+void HAIntegration::stateHandler(bool state, HAFan *sender)
+{
     digitalWrite(LED_BUILTIN, HIGH);
     if (!state)
     {
         analogWrite(0, 0);
         gstate = false;
-    } else {
+    }
+    else
+    {
         analogWrite(0, map(lastspeed, 0, 100, 0, 255));
         gstate = true;
     }
-    sender->setState(state);  // report state back to Home Assistant
+    sender->setState(state); // report state back to Home Assistant
     digitalWrite(LED_BUILTIN, LOW);
 }
 
-void HAIntegration::speedHandler(uint16_t speed, HAFan* sender) {
+void HAIntegration::speedHandler(uint16_t speed, HAFan *sender)
+{
     digitalWrite(LED_BUILTIN, HIGH);
     if (gstate)
     {
         analogWrite(0, map(speed, 0, 100, 0, 255));
     }
-    sender->setSpeed(speed);  // report state back to Home Assistant
+    sender->setSpeed(speed); // report state back to Home Assistant
     lastspeed = speed;
     digitalWrite(LED_BUILTIN, LOW);
 }
 
-void HAIntegration::loop() {
+void HAIntegration::loop()
+{
     mqtt.loop();
+
+    if ((millis() - lastUpdateAt) > 2000)
+    {
+        digitalWrite(dustLed, LOW);
+        delayMicroseconds(280);
+
+        voMeasured = analogRead(A0);
+
+        delayMicroseconds(40);
+
+        digitalWrite(dustLed, HIGH);
+        delayMicroseconds(9680);
+
+        calcVoltage = voMeasured * (3.3 / 1024);
+        dustDensity = 0.17 * calcVoltage - 0.1;
+
+        if (dustDensity < 0)
+        {
+            dustDensity = 0.00;
+        }
+        dustSensor.setValue(dustDensity);
+
+        Serial.println("Sent Update "+String(dustDensity));
+
+        lastUpdateAt = millis();
+    }
 }
